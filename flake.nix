ecb63ef023786ed7fc6f3e113517388d2483946c
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-30 17:36:35 +0200

    Cleanup flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Patched tdesktop";
+  description = "Patched telegram-desktop";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
@@ -9,24 +9,21 @@
     { self, nixpkgs }:
     {
       packages.x86_64-linux.telegram-desktop-unwrapped =
-        pkgs:
-        pkgs.telegram-desktop.unwrapped.overrideAttrs (orig: {
-          patches = (orig.patches or [ ]) ++ [
-            ./patches/hide-premium.patch
-            ./patches/never-show-promo-suggestions.patch
-            ./patches/hide-ai-button.patch
-          ];
-        });
-      packages.x86_64-linux.telegram-desktop =
-        let
+        nixpkgs.legacyPackages.x86_64-linux.telegram-desktop.unwrapped.overrideAttrs
+          (orig: {
+            patches = (orig.patches or [ ]) ++ [
+              ./patches/hide-premium.patch
+              ./patches/never-show-promo-suggestions.patch
+              ./patches/hide-ai-button.patch
+            ];
+          });
 
-          telegram-desktop-with-patches =
-            pkgs:
-            pkgs.telegram-desktop.override ({
-              unwrapped = self.packages.x86_64-linux.telegram-desktop-unwrapped pkgs;
-            });
-        in
-        telegram-desktop-with-patches nixpkgs.legacyPackages.x86_64-linux;
-      packages.x86_64-linux.default = self.packages.x86_64-linux.telegram-desktop-unwrapped nixpkgs.legacyPackages.x86_64-linux;
+      packages.x86_64-linux.telegram-desktop =
+        nixpkgs.legacyPackages.x86_64-linux.telegram-desktop.override
+          ({
+            unwrapped = self.packages.x86_64-linux.telegram-desktop-unwrapped;
+          });
+
+      packages.x86_64-linux.default = self.packages.x86_64-linux.telegram-desktop;
     };
 }
